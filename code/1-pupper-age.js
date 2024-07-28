19b5ea58bcb8dcb3ab675e267c7a1b09cd93ed53
Beth-Savage <savagebe49@craighead.school.nz>
2024-07-29 09:12:33 +1200

    Update 1-pupper-age.js

--- code/1-pupper-age.js
+++ code/1-pupper-age.js
@@ -6,4 +6,8 @@ function pupperAge() {
   alert('Dog years are human years times seven.')
 
   let humanYears = prompt('How old is your dog in human years?')
+  humanYears = Number(humanYears)
+  let dogYears = (humanYears * 7)
+
+  alert('Your dog is ' + dogYears + ' dog years old.')
 }
